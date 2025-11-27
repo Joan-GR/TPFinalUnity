a3e9dea9c73c74f5b0a738e8758736a8464971ff
Joan-GR <joanguerson@gmail.com>
2025-11-27 02:10:45 -0300

    Initial commit

---
+++ Assets/Scripts/FantasmaScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+
+public class FantasmaScript : MonoBehaviour
+{
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Transform objetivo;
+    // Start is called before the first frame update 
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = objetivo.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = objetivo.position;
+        if (agent.remainingDistance < 1)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
